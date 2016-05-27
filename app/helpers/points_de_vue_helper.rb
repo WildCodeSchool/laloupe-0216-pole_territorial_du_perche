78f3fc4e8e6658aac596efd784b2bc319524e921
benjamin <benjamin.lebas@yahoo.fr>
2016-05-27 12:13:14 +0200

    création vignette rouge ou verte

--- app/helpers/points_de_vue_helper.rb
+++ app/helpers/points_de_vue_helper.rb
@@ -2,4 +2,8 @@ module PointsDeVueHelper
   def couleur_bouton(point_de_vue)
     point_de_vue.positif ? "bouton_vert" : "bouton_rouge"
   end
+
+  def couleur_vignette(point_de_vue)
+    point_de_vue.positif ? "vignette_vert" : "vignette_rouge"
+  end
 end
