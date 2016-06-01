5db481a87d24e737ca35a830210346b7253df776
benjamin <benjamin.lebas@yahoo.fr>
2016-06-01 09:32:20 +0200

    réglage de la sécurité

--- app/controllers/contributeurs_controller.rb
+++ app/controllers/contributeurs_controller.rb
@@ -1,5 +1,9 @@
 class ContributeursController < ApplicationController
+
+  after_action :authenticate_animateur!, only: [:show]
+
   def show
     @contributeur = Contributeur.find(params[:id])
   end
+  
 end
