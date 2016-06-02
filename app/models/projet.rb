903c9d4aecb2c20c3a49f83551e96b2a4151ff3a
Alexandre Cuillerier <alexandre.cuillerier@gmail.com>
2016-06-02 15:24:58 +0200

    suppression liens entre projet et ville-codepostal

--- app/models/projet.rb
+++ app/models/projet.rb
@@ -3,8 +3,6 @@ class Projet < ActiveRecord::Base
   has_many :commentaires
   has_many :likes
   belongs_to :contributeur
-  belongs_to :codepostal
-  belongs_to :ville
 
   validates_presence_of :titre, :codepostal,:ville, :description, :contributeur, :categorie
 
