0942173c0491dfe2fe2526fb4238d0b16fffeb2d
Alexandre Cuillerier <alexandre.cuillerier@gmail.com>
2016-06-09 11:47:28 +0200

    plus besoin de modifier le storage des images en fonction de la prod ou du dev

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,9 +6,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
-
-  # storage :fog
-  storage :file
+  if ENV['RAILS_ENV'] == 'production'
+    storage :fog
+  elsif ENV['RAILS_ENV'] == 'development'
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
