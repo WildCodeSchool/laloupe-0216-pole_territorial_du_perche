ae7ae2bbd455f998ba0681abbfd8e80888e99772
alem972 <melanie.gourpil@gmail.com>
2016-06-09 16:13:51 +0200

    creation du controller

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -16,4 +16,18 @@ class QuestionnairesController < ApplicationController
               :code_formulaire)
   end
 
+  def new
+  	@questionnaire = Questionnaire.new	
+  end
+
+  def create
+  	questionnaire = Questionnaire.new(questionnaire_params)
+  	questionnaire.contributeur_id = current_contributeur.id
+  	 if questionnaire.save
+  	 	redirect_to @questionnaire
+  	 else
+      render 'new'
+     end
+  end
+
 end
