8a4acbe1e0e16944c0b1e61bd0107c69456a0deb
benjamin <benjamin.lebas@yahoo.fr>
2016-06-22 10:24:18 +0200

    creation btn supprimer ce résultat

--- app/controllers/reponse_questionnaires_controller.rb
+++ app/controllers/reponse_questionnaires_controller.rb
@@ -1,25 +1,29 @@
 class ReponseQuestionnairesController < ApplicationController
- 	def show
-    @reponse_questionnaire = ReponseQuestionnaire.find(params[:id])
-  	end
-
-  	def new
+ 	
+  def new
   	@reponse_questionnaire = ReponseQuestionnaire.new	
-  	end
+  end
 
-  	def create
+  def create
   	reponse_questionnaire = ReponseQuestionnaire.new(reponse_questionnaire_params)
   	 if reponse_questionnaire.save
-  	 	redirect_to questionnaires_path, method: :get
+  	 	redirect_to new_reponse_questionnaire_path, method: :get
   	 else
       render 'new'
      end
   end
-  private
+
+  def destroy
+    reponse_questionnaire = ReponseQuestionnaire.find(params[:id])
+    reponse_questionnaire.destroy
+    redirect_to questionnaires_path
+  end
+end
+
+private
 
   def reponse_questionnaire_params
     params.require(:reponse_questionnaire)
       .permit(:pourcentage,
               :texte)
   end
-end
